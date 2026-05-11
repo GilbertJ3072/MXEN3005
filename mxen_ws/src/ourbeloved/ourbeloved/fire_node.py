73ea29aed20220656ece7e6d31a8d191bb25855d
GilbertJ3072 <jack.gilbert21@outlook.com>
2026-05-11 11:31:20 +0800

    added fire node and joint state node (fire is bad)

---
+++ mxen_ws/src/ourbeloved/ourbeloved/fire_node.py
@@ -0,0 +1,36 @@
+import rclpy
+from rclpy.executors import ExternalShutdownException
+from rclpy.node import Node
+
+from std_msgs.msg import Bool
+
+
+class fire_node(Node):
+
+    def __init__(self):
+        super().__init__("fire_node")
+        self.subscription = self.create_subscription(Joy, "joy", self.listener_callback, 10)
+        self.publisher = self.create_publisher(Bool, "fire", 10)
+        timer_period = 0.05
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.pressed = False
+
+    def listener_callback(self, msg):
+        msg = Bool()
+        msg.data = True
+        self.publisher.publish(msg)
+        
+
+
+def main(args=None):
+    try:
+        with rclpy.init(args=args):
+            node = fire_node()
+            rclpy.spin(node)
+
+    except (KeyboardInterrupt, ExternalShutdownException):
+        pass
+
+
+if __name__ == '__main__':
+    main()
